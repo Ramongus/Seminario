e2e5d254cf40b6fc8a65e81ad5ab4ad0db4508eb
AgusFont <agustin.gonzalezfont@davinci.edu.ar>
2020-03-31 19:34:06 -0300

    ShaderDamage

---
+++ Assets/DamageMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageMaterial : MonoBehaviour
+{
+    public Material damage;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage();
+        }   
+    }
+    public void Damage()
+    {
+        damage.SetFloat("_Slice", 5);
+        Debug.Log("Danio");
+        StartCoroutine("DamageChange");
+    }
+
+    IEnumerator DamageChange()
+    {
+        damage.SetFloat("_Slice", 5);
+        yield return new WaitForSeconds(2.5f);
+        damage.SetFloat("_Slice", 0);
+    }
+}
