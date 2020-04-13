9d784762b989ff3549cf957c37c258e7bf303f93
Ramiro <ramiro.oliva@davinci.edu.ar>
2020-04-13 02:54:41 -0300

    Correccion-shader y mira

---
+++ Assets/Scripts/CanvasCopyCamaraRot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasCopyCamaraRot : MonoBehaviour
+{
+	private void Start()
+	{
+		transform.forward = Camera.main.transform.forward;
+	}
+}
