c95e1bc3db70d0b6c291a82b349f389495df637d
Ramiro <ramiro.oliva@davinci.edu.ar>
2020-05-16 11:41:54 -0300

    Antes de cambio de spells

---
+++ Assets/Scripts/Enemys/EnemyWrapper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWrapper : MonoBehaviour
+{
+	EnemyModel _model;
+	EnemyView _view;
+
+	private void Awake()
+	{
+		_model = new EnemyModel();
+		_view = new EnemyView();
+	}
+
+	private void Update()
+	{
+		_model.UpdateModel();
+	}
+}
+
+public class EnemyModel
+{
+	StateMachineClassic _sm;
+	public EnemyModel()
+	{
+		_sm = new StateMachineClassic();
+		_sm.AddState(new ChangeToStateAfterSeconds<EmptyStateTest>(_sm, 10));
+		_sm.AddState(new EmptyStateTest(_sm));
+	}
+
+	public void UpdateModel()
+	{
+		_sm.Update();
+	}
+}
+
+public class EnemyView
+{
+
+}
+
+public class EmptyStateTest : State
+{
+	public EmptyStateTest(StateMachineClassic sm) : base(sm)
+	{
+	}
+
+	public override void Awake()
+	{
+		base.Awake();
+		Debug.Log("EmptyState");
+	}
+}
+
+public class ChangeToStateAfterSeconds<T> : State where T : State
+{
+	float secondsCharging;
+
+	float timer;
+
+	public ChangeToStateAfterSeconds(StateMachineClassic sm, float timeCharging) : base(sm)
+	{
+		secondsCharging = timeCharging;
+		timer = secondsCharging;
+	}
+
+	public override void Awake()
+	{
+		timer = secondsCharging;
+	}
+
+	public override void Execute()
+	{
+		timer -= Time.deltaTime;
+		Debug.Log("Timer: " + timer);
+		if (timer <= 0)
+			_sm.SetState<T>();
+	}
+}
+
+public class MetheoriteAttackState : State
+{
+
+
+	public MetheoriteAttackState(StateMachineClassic sm) : base(sm)
+	{
+	}
+
+	public override void Awake()
+	{
+		base.Awake();
+		CreateBombs();
+	}
+
+	private void CreateBombs()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/ObjectInstantiator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInstantiator : MonoBehaviour
+{
+
+	private void Awake()
+	{
+		EventsManager.SuscribeToEvent("Instantiate", InstantiateObject);
+	}
+
+	public void InstantiateObject(params object[] parameters)
+	{
+
+	}
+}
