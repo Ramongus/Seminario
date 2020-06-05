4bd5cfa3f0fdd2977304cdd4133e05f1a4a2829e
Ramiro <ramiro.oliva@davinci.edu.ar>
2020-06-05 02:26:47 -0300

    Push-9Niveles-Jugables

---
+++ Assets/Scripts/Portales/ConnectedPortals.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectedPortals : MonoBehaviour
+{
+	[SerializeField] ConnectedPortals portalConnected;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Player player = other.GetComponent<Player>();
+		if(player != null)
+		{
+			portalConnected.GetComponent<Collider>().enabled = false;
+			player.transform.position = new Vector3(portalConnected.transform.position.x, player.transform.position.y, portalConnected.transform.position.z);
+		}
+	}
+}
