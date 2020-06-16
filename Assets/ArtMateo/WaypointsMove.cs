af4b3c9537bbaa8d0aff44461c55101eaad92d22
MateoEz <57017574+MateoEz@users.noreply.github.com>
2020-06-16 11:11:46 -0300

    COMMIT

---
+++ Assets/ArtMateo/WaypointsMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsMove : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float minimumDistance;
+    private int currentIndex;
+    private bool goBack;
+
+    public List<Transform> waypoints;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        Vector3 deltaVector = waypoints[currentIndex].position - transform.position;
+        Vector3 direction = deltaVector.normalized;
+
+        transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * rotationSpeed);
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        float distance = deltaVector.magnitude;
+        if (distance < minimumDistance)
+        {
+            currentIndex = goBack ? --currentIndex : ++currentIndex;
+            if (currentIndex >= waypoints.Count)
+            {
+                currentIndex = 0;
+            }
+            else if (currentIndex <= 0)
+            {
+                goBack = false;
+            }
+        }
+    }
+}
